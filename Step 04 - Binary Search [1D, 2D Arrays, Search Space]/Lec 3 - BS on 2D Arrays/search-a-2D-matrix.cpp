66ea703b4137d072e23c9db58129cc53746dbdae
Megh Bari <meghbari01@gmail.com>
2025-01-09 20:57:53 +0530

    BS | Search in 2D Matrix Part-I

--- Step 04 - Binary Search [1D, 2D Arrays, Search Space]/Lec 3 - BS on 2D Arrays/search-a-2D-matrix.cpp
+++ Step 04 - Binary Search [1D, 2D Arrays, Search Space]/Lec 3 - BS on 2D Arrays/search-a-2D-matrix.cpp
@@ -52,19 +52,28 @@ bool searchMatrix(vector<vector<int>>& matrix, int target) {
 
 
 // optimal: BS 
-bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        
-        int row = 0, col = m - 1;
 
-        while (row < n && col >= 0) {
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+      int n = matrix.size(); 
+        int m = matrix[0].size();
+
+        int low = 0, high = n * m - 1; 
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2; 
+            int row = mid / m; 
+            int col = mid % m; 
+
             if (matrix[row][col] == target) {
                 return true;
-            } else if (matrix[row][col] < target) {
-                row++; 
-            } else {
-                col--;  
+            } 
+           
+            else if (matrix[row][col] < target) {
+                low = mid + 1;
+            } 
+           
+            else {
+                high = mid - 1;
             }
         }
 
