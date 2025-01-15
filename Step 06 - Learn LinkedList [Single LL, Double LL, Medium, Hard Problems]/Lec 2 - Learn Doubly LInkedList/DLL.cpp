5f5ff39c27b314a5f128eb7566f0150ed039885f
Megh Bari <meghbari01@gmail.com>
2025-01-15 21:15:27 +0530

    DLL insert, delete

--- Step 06 - Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 2 - Learn Doubly LInkedList/DLL.cpp
+++ Step 06 - Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 2 - Learn Doubly LInkedList/DLL.cpp
@@ -98,16 +98,175 @@ Node *removeTail(Node *head)
     return head;
 }
 
+// delete the kth element
+
+Node *removeKthElement(Node *head, int k)
+{
+    if (head == NULL)
+        return NULL;
+
+    int cnt = 0;
+    Node *kNode = head;
+
+    while (kNode != NULL)
+    {
+        cnt++;
+        if (cnt == k)
+            break;
+
+        kNode = kNode->next;
+    }
+
+    Node *prev = kNode->back;
+    Node *front = kNode->next;
+
+    if (prev == NULL && front == NULL)
+    {
+        return NULL;
+    }
+    else if (prev == NULL)
+    {
+        return removeHead(head);
+    }
+    else if (front == NULL)
+    {
+        return removeTail(head);
+    }
+
+    prev->next = front;
+    front->back = prev;
+
+    kNode->next = NULL;
+    kNode->back = NULL;
+
+    delete kNode;
+
+    return head;
+}
+
+// delete Node from the DLL
+
+void remoeveNode(Node *temp)
+{
+    Node *prev = temp->back;
+    Node *front = temp->next;
+
+    if (front == NULL)
+    {
+        prev->next = nullptr;
+        temp->back = nullptr;
+        free(temp);
+
+        return;
+    }
+
+    prev->next = front;
+    front->back = prev;
+
+    temp->next = temp->back = nullptr;
+
+    free(temp);
+}
+
+// insertion head (before)
+
+Node *insertBeforeHead(Node *head, int val)
+{
+    Node *newHead = new Node(val, head, nullptr);
+    head->back = newHead;
+    return newHead;
+}
+// insertion tail (before)
+
+Node *insertBeforeTail(Node *head, int val)
+{
+    if (head->next == NULL)
+    {
+        return insertBeforeHead(head, val);
+    }
+    Node *tail = head;
+
+    while (tail->next != NULL)
+    {
+        tail = tail->next;
+    }
+    Node *prev = tail->back;
+    Node *newNode = new Node(val, tail, prev);
+    prev->next = newNode;
+    tail->back = newNode;
+    return head;
+}
+
+// insertion kth element (before)
+
+Node *insertBeforeKthElement(Node *head, int k, int val)
+{
+    if (k == 1)
+    {
+        return insertBeforeHead(head, val);
+    }
+    Node *temp = head;
+    int cnt = 0;
+    while (temp != NULL)
+    {
+        cnt++;
+
+        if (cnt == k)
+            break;
+        temp = temp->next;
+    }
+    Node *prev = temp->back;
+    Node *newNode = new Node(val, temp, prev);
+
+    prev->next = newNode;
+    temp->back = newNode;
+
+    return head;
+}
+
+// insert element in given node (before)
+
+void insertBeforeGivenNode(Node* node, int val){
+    Node* prev = node->back;
+    Node* newNode = new Node(val, node, prev);
+
+    prev->next = newNode;
+    node->back = newNode;
+}
+
 int main()
 {
 
     vector<int> arr = {12, 5, 8, 7};
     Node *head = convertArryToDLL(arr);
+
+    // deletion
+
     // print(head);
     // head = removeHead(head);
     // print(head);
 
-    head = removeTail(head);
+    // head = removeTail(head);
+    // print(head);
+
+    // head = removeKthElement(head, 1);
+    // print(head);
+
+    // remoeveNode(head->next->next);
+    // print(head);
+
+    // insertion
+
+    // head = insertBeforeHead(head, 10);
+    // print(head);
+
+    // head = insertBeforeTail(head, 20);
+    // print(head);
+
+    // head = insertBeforeKthElement(head, 3, 20);
+    // print(head);
+
+    insertBeforeGivenNode(head->next, 100);
     print(head);
 
     return 0;
