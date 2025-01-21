043786cad15d7084824bbc7d235e754c5e55866b
Megh Bari <meghbari01@gmail.com>
2025-01-21 21:50:36 +0530

    LL | delete the middle node of linkedList

---
+++ Step 06 - Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 3 - Medium Problems of LL/delete-the-middle-node-of-linkedList.cpp
@@ -0,0 +1,18 @@
+ListNode* deleteMiddle(ListNode* head) {
+        if (head == NULL || head->next == NULL)
+            return NULL;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        fast = fast->next->next;
+
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* midNode = slow->next;
+        slow->next = slow->next->next;
+        delete midNode;
+        return head;
+    }
