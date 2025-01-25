aad29bf40e40c2cbb864718e6901210c19c072e8
Megh Bari <meghbari01@gmail.com>
2025-01-25 21:08:58 +0530

    LL | reverse node in kth group

---
+++ Step 06 - Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 5 - Hard Problems of LL/reverse-nodes-in-kth-group.cpp
@@ -0,0 +1,60 @@
+Node *reverseLinkedList(Node *head){
+    Node *temp = head;
+    Node *prev = NULL;
+
+    while (temp != NULL){
+        Node *front = temp->next;
+        temp->next = prev;
+
+        prev = temp;
+        temp = front;
+    }
+    return prev;
+}
+
+Node *findKthNode(Node *temp, int k){
+    k -= 1;
+
+    while (temp != NULL && k > 0){
+        k--;
+        temp = temp->next;
+    }
+    return temp;
+}
+
+Node *kReverse(Node *head, int k){
+    Node *temp = head;
+
+    Node *prevLast = NULL;
+
+    while (temp != NULL){
+        Node *kthNode = findKthNode(temp, k);
+
+        if (kthNode == NULL){
+
+            if (prevLast){
+                prevLast->next = kthNode;
+            }
+
+            break;
+        }
+
+        Node *nextNode = kthNode->next;
+
+        kthNode->next = NULL;
+
+        reverseLinkedList(temp);
+
+        if (temp == head){
+            head = kthNode;
+        }
+        else{
+            prevLast->next = kthNode;
+        }
+
+        prevLast = temp;
+
+        temp = nextNode;
+    }
+    return head;
+}
