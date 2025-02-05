e02d01b2e4b5194d68c1d0c02366164f42dcb1ec
Megh Bari <meghbari01@gmail.com>
2025-02-05 20:17:27 +0530

    Recursion | N-Queens (using hashing)

--- Step 07 - Recursion [PatternWise]/Lec 3 - Trying out all Combos  Hard/N-Queen.cpp
+++ Step 07 - Recursion [PatternWise]/Lec 3 - Trying out all Combos  Hard/N-Queen.cpp
@@ -65,3 +65,41 @@ vector<vector<string>> solveNQueens(int n)
     solve(0, board, ans, n);
     return ans;
 }
+
+//  optimal using hashing 
+
+   void solve(int col, vector<string>&board, vector<vector<string>>&ans, vector<int>&leftRow, vector<int>&upperDiagonal, vector<int> &lowerDiagonal, int n){
+    if(col==n){
+        ans.push_back(board);
+        return;
+    }
+    for(int row=0; row<n; row++){
+        // small checks
+        if(leftRow[row]==0 && lowerDiagonal[row+col]==0
+        && upperDiagonal[n-1+col-row]==0){
+            // marked the queen
+            board[row][col]='Q';
+            leftRow[row]=1;
+            lowerDiagonal[row+col]=1;
+            upperDiagonal[n-1+col-row]=1;
+            // then recursion
+            solve(col+1, board, ans, leftRow, upperDiagonal, lowerDiagonal,n);
+            // unmarked the queen
+            board[row][col] ='.';
+            leftRow[row]=0;
+            lowerDiagonal[row+col]=0;
+            upperDiagonal[n-1+col-row]=0;
+        }
+    }
+   }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n, '.');
+        for (int i = 0; i < n; i++) {
+            board[i] = s;
+        }
+        vector<int>leftRow(n,0), upperDiagonal(2*n-1,0), lowerDiagonal(2*n-1,0);
+        solve(0, board, ans, leftRow, upperDiagonal, lowerDiagonal, n);
+        return ans;
+    }
