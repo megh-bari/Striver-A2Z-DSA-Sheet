d6ac026f92d11ae8b2f3e65c2b5cf25593ff98b9
Megh Bari <meghbari01@gmail.com>
2025-02-26 21:06:00 +0530

    one liner code for short notes

---
+++ Step 09 - Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 2 - Prefix, Infix, PostFix Conversion Problems/one-liner-code.cpp
@@ -0,0 +1,7 @@
+postfix to infix = iterate left to right and used '('+ t2 + s[i] + t1 + ')'
+
+prefix to infix = iterate from right to left and used '(' + t1 + s[i] + t2 + ')'
+
+postfix to prefix = iterate from left to right and used  operator + t2 + t1
+
+prefix to postfix = iterate from right to left and used t1 + t2 + operator 
