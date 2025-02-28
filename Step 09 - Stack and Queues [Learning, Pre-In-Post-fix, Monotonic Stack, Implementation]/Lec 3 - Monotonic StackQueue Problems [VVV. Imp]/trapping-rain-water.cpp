ee04b500806955629155ae50d71096f5129b32f1
Megh Bari <meghbari01@gmail.com>
2025-02-28 21:03:55 +0530

    trapping rain water - approach 1

---
+++ Step 09 - Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 3 - Monotonic StackQueue Problems [VVV. Imp]/trapping-rain-water.cpp
@@ -0,0 +1,42 @@
+// not much optimal 
+class Solution {
+    public:
+        vector<int> prefixMax(vector<int>& arr) {
+            int n = arr.size();
+            vector<int> prefix(n);
+            prefix[0] = arr[0];
+            for (int i = 1; i < n; i++) {
+                prefix[i] = max(prefix[i - 1], arr[i]);
+            }
+    
+            return prefix;
+        }
+    
+        vector<int> suffixMax(vector<int>& arr) {
+            int n = arr.size();
+            vector<int> suffix(n);
+            suffix[n - 1] = arr[n - 1];
+    
+            for (int i = n - 2; i >= 0; i--) {
+                suffix[i] = max(suffix[i + 1], arr[i]);
+            }
+    
+            return suffix;
+        }
+        int trap(vector<int>& height) {
+            int total = 0;
+            int n = height.size();
+            if (n == 0)
+                return 0;
+            vector<int> leftMax = prefixMax(height);
+            vector<int> rightMax = suffixMax(height);
+    
+            for (int i = 0; i < n; i++) {
+    
+                if (height[i] < leftMax[i] && height[i] < rightMax[i]) {
+                    total += min(leftMax[i], rightMax[i]) - height[i];
+                }
+            }
+            return total;
+        }
+    };
