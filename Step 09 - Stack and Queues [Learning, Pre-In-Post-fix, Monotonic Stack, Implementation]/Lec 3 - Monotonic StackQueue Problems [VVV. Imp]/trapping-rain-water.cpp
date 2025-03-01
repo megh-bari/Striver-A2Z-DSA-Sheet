cd74a98b3e4045671028e2ec4688993f93df8b3f
Megh Bari <meghbari01@gmail.com>
2025-03-01 20:52:23 +0530

    trapping rain water - approach 2 (optimal)

--- Step 09 - Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 3 - Monotonic StackQueue Problems [VVV. Imp]/trapping-rain-water.cpp
+++ Step 09 - Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 3 - Monotonic StackQueue Problems [VVV. Imp]/trapping-rain-water.cpp
@@ -40,3 +40,34 @@ class Solution {
             return total;
         }
     };
+
+// optimal 
+class Solution {
+    public:
+        int trap(vector<int>& height) {
+            if (height.empty()) return 0;  
+            
+            int total = 0;
+            int l = 0, r = height.size() - 1; 
+            int lMax = 0, rMax = 0;  
+            
+            while (l < r) {
+                if (height[l] <= height[r]) {
+                    if (height[l] >= lMax) 
+                        lMax = height[l];
+                    else 
+                        total += lMax - height[l];
+                    l++;  
+                } else {
+                    if (height[r] >= rMax) 
+                        rMax = height[r];
+                    else 
+                        total += rMax - height[r];
+                    r--;  
+                }
+            }
+            
+            return total;
+        }
+    };
+    
