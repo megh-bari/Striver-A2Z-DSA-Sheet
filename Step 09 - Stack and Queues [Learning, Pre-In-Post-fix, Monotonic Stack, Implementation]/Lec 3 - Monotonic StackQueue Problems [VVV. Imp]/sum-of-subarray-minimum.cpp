d6ce1ea1fa4d4dac05d70675cc3767828bfe4a0e
Megh Bari <meghbari01@gmail.com>
2025-03-02 19:54:45 +0530

    sum of subarray minimum - optimal

--- Step 09 - Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 3 - Monotonic StackQueue Problems [VVV. Imp]/sum-of-subarray-minimum.cpp
+++ Step 09 - Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 3 - Monotonic StackQueue Problems [VVV. Imp]/sum-of-subarray-minimum.cpp
@@ -17,3 +17,51 @@ class Solution {
                 return sum;
         }
     };
+
+// optimal 
+class Solution {
+    public:
+        vector<int> findNSE(vector<int>& arr) {
+            int n = arr.size();
+            stack<int> st;
+            vector<int> nse(n, n);
+            for (int i = n - 1; i >= 0; i--) {
+                while (!st.empty() && arr[st.top()] >= arr[i]) {
+                    st.pop();
+                }
+                nse[i] = st.empty() ? n : st.top();
+                st.push(i);
+            }
+            return nse;
+        }
+        
+        vector<int> findPSE(vector<int>& arr) {
+            int n = arr.size();
+            stack<int> st;
+            vector<int> pse(n, -1);
+            for (int i = 0; i < n; i++) {
+                while (!st.empty() && arr[st.top()] > arr[i]) {
+                    st.pop();
+                }
+                pse[i] = st.empty() ? -1 : st.top();
+                st.push(i);
+            }
+            return pse;
+        }
+    
+        int sumSubarrayMins(vector<int>& arr) {
+            int n = arr.size();
+            vector<int> nse = findNSE(arr);
+            vector<int> pse = findPSE(arr);
+            long long total = 0;
+            int mod = (int)(1e9 + 7);
+            
+            for (int i = 0; i < n; i++) {
+                long long left = i - pse[i];
+                long long right = nse[i] - i;
+                total = (total + arr[i] * left * right) % mod;
+            }
+            return (int)total;
+        }
+    };
+    
