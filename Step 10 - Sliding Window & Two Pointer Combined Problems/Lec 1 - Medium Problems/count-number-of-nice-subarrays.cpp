4dd09e4a66d8c1fb1e22f2a80e58dec33d0059f7
Megh Bari <meghbari01@gmail.com>
2025-03-25 20:48:06 +0530

    count number of nice subarray

---
+++ Step 10 - Sliding Window & Two Pointer Combined Problems/Lec 1 - Medium Problems/count-number-of-nice-subarrays.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+
+class Solution {
+public:
+    int answer(vector<int>& nums, int k) {
+        int n = nums.size();
+        int l = 0;
+        int r = 0;
+        int count = 0;
+
+        if (k < 0)
+            return 0;
+
+        // odd numbers
+        for (int r = 0; r < n; r++) {
+            if (nums[r] % 2 == 1) {
+                k--;
+            }
+
+            while (k < 0) {
+                if (nums[l] % 2 == 1) {
+                    k++;
+                }
+                l++;
+            }
+
+            count += (r - l + 1);
+        }
+        return count;
+    }
+
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return answer(nums, k) - answer(nums, k - 1);
+    }
+};
